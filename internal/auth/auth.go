5848fffc321e17c931359787f90e519943cc6062
nickplusdrums <nickplusdrums@gmail.com>
2026-04-20 09:27:27 -0700

    Add new JWT function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,6 +3,9 @@ package auth
 import (
 	"github.com/alexedwards/argon2id"
 	"errors"
+	"time"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -19,4 +22,24 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, errors.New("Failed to Call Compare Password and Hash")
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	claims := jwt.RegisteredClaims{
+		Issuer: "chirpy-access",
+		IssuedAt:	jwt.NewNumericDate(time.Now()),
+		ExpiresAt:	jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:	userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	//
+
+
 }
